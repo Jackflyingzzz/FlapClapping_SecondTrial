3185a65ea8425b619c6a557bcd8ce3a50c032b89
Jackflyingzzz <105610300+Jackflyingzzz@users.noreply.github.com>
2023-09-01 15:55:56 +0100

    Update probes.py

--- rl-training/probes.py
+++ rl-training/probes.py
@@ -6,6 +6,7 @@ class ProbeLayout():
 	def __init__(self, probe_params, solver_params):
 		self.s_params = solver_params
 		self.layout_type = probe_params['type']
+		self.n_probes = probe_params['n_probes']
 
 	def generate_probe_layout_file(self, output_file):
 		probe_coords = None
@@ -40,14 +41,30 @@ class ProbeLayout():
 			probe_coords = np.array(np.meshgrid(x_probe_space, y_probe_space), dtype=int) # TODO: We dont need meshgrid, just iterate over two linspaces
 			self.n_probes = 200
 
+		elif self.layout_type == 'uniform_base':
+			back_face_mesh_x = np.ceil(self.s_params.mesh_x * (self.s_params.x_centre + self.s_params.ar/2 + margin) / self.s_params.domain_x)
+			top_face_y = self.s_params.y_centre + 1/2
+			bottom_face_y = self.s_params.y_centre - 1/2
+
+			top_face_mesh_point_y = np.argmin(abs(top_face_y - self.s_params.refined_mesh_y_pts))
+			bottom_face_mesh_point_y = np.argmin(abs(bottom_face_y - self.s_params.refined_mesh_y_pts))
+
+			x_probe_location = back_face_mesh_x
+			y_probe_location = np.linspace(bottom_face_mesh_point_y, top_face_mesh_point_y, self.n_probes, dtype=int)
+			
+
 		else:
 			raise NotImplementedError
 
 		if probe_coords is not None:
 			out = open(output_file, 'w')
-			for i in range(probe_coords.shape[1]): # Loop over probe_coords or flatten to obtain pairs of (x, y) coordinates then write into a file to generate probes
-				for j in range(probe_coords.shape[2]):
-					out.write(f'{probe_coords[0, i, j]} {probe_coords[1, i, j]} 1\n')
+			if self.layout_type == 'uniform_base':
+				for i in range(y_probe_location.shape[0]):
+					out.write(f'{x_probe_location} {y_probe_location{i} 1\n')
+			else:
+				for i in range(probe_coords.shape[1]): # Loop over probe_coords or flatten to obtain pairs of (x, y) coordinates then write into a file to generate probes
+					for j in range(probe_coords.shape[2]):
+						out.write(f'{probe_coords[0, i, j]} {probe_coords[1, i, j]} 1\n')
 			out.close()
 	
 	def get_n_probes(self):
